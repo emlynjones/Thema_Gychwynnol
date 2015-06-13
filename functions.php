6969d1bdb5bc7e4cfdc0ef6f4e1bc5d0a631d44c
emlynjones <emlynjones121@gmail.com>
2015-06-13 10:27:03 +0100

    Addasu Steil i fod fwy plaen & debyg i wefan

--- functions.php
+++ functions.php
@@ -22,6 +22,78 @@ function thema_gychwynnol_setup() {
 	 * to change 'thema_gychwynnol' to the name of your theme in all the template files
 	 */
 	load_theme_textdomain( 'thema_gychwynnol', get_template_directory() . '/languages' );
+	
+	//Mynnu bod y plugin "Polylang" yn cael ei lwytho
+	require_once dirname( __FILE__ ) . '/required-plugins/class-tgm-plugin-activation.php';
+
+	add_action( 'tgmpa_register', 'thema_gychwynnol_register_required_plugins' );
+	
+	function thema_gychwynnol_register_required_plugins() {
+
+    /**
+     * Array of plugin arrays. Required keys are name and slug.
+     * If the source is NOT from the .org repo, then source is also required.
+     */
+    $plugins = array(
+
+        // Yr ategion sydd angen eu llwytho o repository WordPress
+        array(
+            'name'      => 'Polylang',
+            'slug'      => 'polylang',
+            'required'  => true,
+        ),
+
+    );
+
+    /**
+     * Array of configuration settings. Amend each line as needed.
+     * If you want the default strings to be available under your own theme domain,
+     * leave the strings uncommented.
+     * Some of the strings are added into a sprintf, so see the comments at the
+     * end of each line for what each argument will be.
+     */
+    $config = array(
+        'default_path' => '',                      // Default absolute path to pre-packaged plugins.
+        'menu'         => 'tgmpa-install-plugins', // Menu slug.
+        'has_notices'  => true,                    // Show admin notices or not.
+        'dismissable'  => false,                    // If false, a user cannot dismiss the nag message.
+        'dismiss_msg'  => 'Rhaid cael yr ategyn yma wedi ei weithredu.',                      // If 'dismissable' is false, this message will be output at top of nag.
+        'is_automatic' => true,                   // Automatically activate plugins after installation or not.
+        'message'      => 'Llwytho yr ategion angenrheidiol.',                      // Message to output right before the plugins table.
+        'strings'      => array(
+            'page_title'                      => __( 'Install Required Plugins', 'tgmpa' ),
+            'menu_title'                      => __( 'Install Plugins', 'tgmpa' ),
+            'installing'                      => __( 'Installing Plugin: %s', 'tgmpa' ), // %s = plugin name.
+            'oops'                            => __( 'Something went wrong with the plugin API.', 'tgmpa' ),
+            'notice_can_install_required'     => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.' ), // %1$s = plugin name(s).
+            'notice_can_install_recommended'  => _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.' ), // %1$s = plugin name(s).
+            'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.' ), // %1$s = plugin name(s).
+            'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s).
+            'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s).
+            'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.' ), // %1$s = plugin name(s).
+            'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.' ), // %1$s = plugin name(s).
+            'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.' ), // %1$s = plugin name(s).
+            'install_link'                    => _n_noop( 'Begin installing plugin', 'Begin installing plugins' ),
+            'activate_link'                   => _n_noop( 'Begin activating plugin', 'Begin activating plugins' ),
+            'return'                          => __( 'Return to Required Plugins Installer', 'tgmpa' ),
+            'plugin_activated'                => __( 'Plugin activated successfully.', 'tgmpa' ),
+            'complete'                        => __( 'All plugins installed and activated successfully. %s', 'tgmpa' ), // %s = dashboard link.
+            'nag_type'                        => 'updated' // Determines admin notice type - can only be 'updated', 'update-nag' or 'error'.
+        )
+    );
+
+    tgmpa( $plugins, $config );
+
+}/*
+A oes modd cael yr ieithoedd wedi eu gosod yn awtomatig yn Polylang?
+*/
+
+
+
+/*
+--------------------------------------------------------------------
+*/
+
 
 	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
@@ -141,3 +213,129 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+
+
+
+/*
+
+Adio tudalen groeso unwaith mae'r thema wedi ei weithredu.
+
+Gweler yma - http://premium.wpmudev.org/blog/plugin-welcome-screen/
+
+*/
+
+
+//register_activation_hook( __FILE__, 'techcy_gweithredu_tud_groeso' );
+add_action("after_switch_theme", "techcy_gweithredu_tud_groeso", 10 ,  2);
+
+function techcy_gweithredu_tud_groeso() {
+	set_transient( '_tud_groeso_ailgyfeirio_gweithredu', true, 30 );
+}
+
+add_action( 'admin_init', 'techcy_ailgyfeirio_tud_groeso_ar_weithredu' );
+
+function techcy_ailgyfeirio_tud_groeso_ar_weithredu() {
+
+	// Bail if no activation redirect
+	if ( !get_transient( '_tud_groeso_ailgyfeirio_gweithredu' ) ) {
+		return;
+	}
+
+	// Delete the redirect transient
+	delete_transient( '_tud_groeso_ailgyfeirio_gweithredu' );
+
+	// Bail if activating from network, or bulk
+	if ( is_network_admin() || isset( $_GET['activate-multi'] ) ) {
+		return;
+	}
+
+	// Ailgyfeirio i dudalen groeso
+	wp_safe_redirect( add_query_arg( array( 'page' => 'tudalen-groeso-techcy' ), admin_url( 'index.php' ) ) );
+
+}
+
+add_action('admin_menu', 'techcy_tudalen_groeso');
+
+function techcy_tudalen_groeso() {
+	add_dashboard_page(
+	'Croeso i Thema XXX',
+	'Croeso i Thema XXX',
+	'read',
+	'tudalen-groeso-techcy',
+	'cynnwys_tud_groeso'
+	);
+}
+
+function cynnwys_tud_groeso() {
+
+	?>
+	<style>
+	.techcy-tudgroeso-cynhwysyn{
+		margin:100px auto;
+		width: 80%;
+		border: 0px solid black;
+		}
+	.techcy-tudgroeso-trydydd{
+		width:250px;
+		float:left;
+	}
+	</style>
+	<div class = "techcy-tudgroeso-cynhwysyn">
+	<?php
+	echo '<h1>Croeso i Thema XXX</h1>'
+	?>
+	<p>Diolch am ddewis y thema yma.</p>
+	
+	<div class = "techcy-tudgroeso-trydydd">
+	<h2>Cychwyn</h2>
+	</div>
+
+	<div class = "techcy-tudgroeso-trydydd">
+	<h2>Help</h2>
+	</div>
+
+	<div class = "techcy-tudgroeso-trydydd">
+	<h2>Tiwtorialau</h2>
+	</div>
+
+
+
+
+	</div>
+	<?php
+	}
+
+	
+//add_action( 'admin_head', 'techcy_tynnu_tud_groeso_o_ddewislen' );
+	
+function techcy_tynnu_tud_groeso_o_ddewislen() {
+	remove_submenu_page( 'index.php', 'tudalen-groeso-techcy' );
+}
+
+
+
+/*
+
+Adio Paneli yn y bwrdd dash.
+
+*/
+add_action('wp_dashboard_setup', 'thema_gychwynnol_gosod_panel_dashfwrdd');
+
+function thema_gychwynnol_gosod_panel_dashfwrdd() {
+	
+	global $wp_meta_boxes;
+
+	$teitl_dashfwrdd_help = __("Help a'r Thema");
+	
+	add_meta_box('thema_gychwynnol_dash_help', $teitl_dashfwrdd_help, 'thema_gychwynnol_dashfwrdd_help', 'dashboard', 'side', 'high');
+
+}
+
+
+function thema_gychwynnol_dashfwrdd_help() {
+	echo '<h1>Croeso i Thema Ddwyieithog</h1>';
+	echo "<p>Llongyfarchiadau - mae'r thema yma wedi ei weithredu.";
+	echo '<iframe width="100%" height = "300px" src="https://www.youtube.com/embed/w9OhG7Wx1CY" frameborder="0" allowfullscreen></iframe>';
+}
+
